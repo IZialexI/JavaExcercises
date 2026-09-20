56bb2da80a55f25e4b49a22dfc22fcaada806c61
IIIZialexIII <david.cabgon@gmail.com>
2026-09-20 10:32:14 +0200

    feat(JavaVariablesAndConstants): add part one of JavaWhoAmI with main variables types

---
+++ JavaVariablesAndConstants/JavaVariablesAndConstants.java
@@ -0,0 +1,16 @@
+public class JavaVariablesAndConstants {
+	public static void main(String[]args) {
+		// String for the name
+		// final because it's a constant
+		// Constants never change
+		final String MY_NAME = "Zia";
+		// int for the age
+		// It's a variable, the value can change
+		int myAge = 20;
+		// double for the height, it has decimals
+		double myHeight = 1.75;
+		// boolean for the state, only two options
+		boolean isStudying = true;
+		System.out.println("This is part one of JavaWhoAmI, in the part two, we will recycle this code and add input");
+	}
+}
