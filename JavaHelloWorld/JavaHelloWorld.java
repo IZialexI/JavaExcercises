08c65b23625bbe4a7e189a072ee56ecfc4bd367c
IIIZialexIII <david.cabgon@gmail.com>
2026-09-20 08:38:56 +0200

    feat: Hello World added

---
+++ JavaHelloWorld/JavaHelloWorld.java
@@ -0,0 +1,10 @@
+// The class' name must be the same as the file's name
+// It contains the full program
+public class JavaHelloWorld {
+	// Main function
+	public static void main (String[] args) {
+		// To print `Hello World!` on the CLI
+		// ln prints an Enter after the string
+		System.out.println ("Hello World!");
+	}
+}
